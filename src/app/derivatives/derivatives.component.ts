0a795938f5327e5a84b714df179af607c388efab
shareefsk1 <sharifsk9985@gmail.com>
2022-12-10 13:29:57 +0530

    attribute Directives are commited([ngStyle and [ngClass]])

--- src/app/derivatives/derivatives.component.ts
+++ src/app/derivatives/derivatives.component.ts
@@ -11,5 +11,12 @@ export class DerivativesComponent {
     public time :number = 19;
 
 
+    public head : any = ['Name',"Age",'Package','Role']
+
+    public table : any = [{name:'shareef',age:24,package:'40',Role:'fullstack'},
+                          {name:'shareef1',age:15,package:'15',Role:'fullstack'},
+                          {name:'shareef2',age:30,package:'50',Role:'fullstack'},
+                          {name:'shareef3',age:21,package:'20',Role:'fullstack'},
+                          {name:'shareef4',age:35,package:'80',Role:'fullstack'}]
 
   }
